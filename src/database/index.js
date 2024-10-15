5121c2e5f83b6ba3f597a423f3d70d93dfadb3bf
andremessa <messa.andre@gmail.com>
2024-10-14 23:30:56 -0300

    criado schema de pedidos

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import confingDatabase from '../config/database';
 
@@ -11,12 +12,19 @@ const models = [User, Product, Category];
 class Database {
 	constructor() {
 		this.init();
+		this.mongo();
 	}
 
 	init() {
 		this.connection = new Sequelize(confingDatabase);
 		// biome-ignore lint/complexity/useOptionalChain: <explanation>
 		models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
+	}
+
+	mongo() {
+		this.mongoConnection = mongoose.connect(
+			'mongodb://localhost:27017/devburger'
+		)
 	}
 }
 
