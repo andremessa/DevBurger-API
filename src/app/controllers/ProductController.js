b6004de439a7f7cd721a3106965b83796d03ca35
andremessa <messa.andre@gmail.com>
2024-10-17 00:14:53 -0300

    Status de pedido e validação do usuario

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 import Product from '../models/Product';
 import Category from '../models/Category';
+import User from '../models/User';
 
 class ProductController {
 	async store(request, response) {
@@ -14,6 +15,12 @@ class ProductController {
 			schema.validateSync(request.body, { abortEarly: false });
 		} catch (err) {
 			return response.status(400).json({ erros: err.errors });
+		}
+
+		const { admin: isAdmin } = await User.findByPk(request.userId);
+
+		if (!isAdmin) {
+			return response.status(401).json();
 		}
 
 		const { filename: path } = request.file;
@@ -31,12 +38,13 @@ class ProductController {
 
 	async index(request, response) {
 		const products = await Product.findAll({
-			include: [{
-				model: Category,
-				as: 'category',
-				attributes: ['id', 'name']
-			}
-		]
+			include: [
+				{
+					model: Category,
+					as: 'category',
+					attributes: ['id', 'name'],
+				},
+			],
 		});
 
 		return response.json(products);
